65a0e26ba8f3d9c525fc2a39248dd8305799f767
Eric <erictaw2018@u.northwestern.edu>
2016-03-27 14:31:28 -0500

    load json

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-var data = require('./data.json');
+function loadJSON(callback) {
 
-console.log(data);
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+
+        }
+    }
+    xobj.send(null);
+}
+
+loadJSON(function(response) {
+    var data = JSON.parse(response);
+    var nodes = data.nodes;
+    var links = data.links;
+    console.log(data)
+})
