168e3c1d3a7d9ef6822f8809e4c8d5358843c4c6
Eric <erictaw2018@u.northwestern.edu>
2016-03-28 08:27:14 -0500

    add cubehelix.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
 
     function visualize(data) {
         var width = 960;
-        height = 500;
+        var height = 500;
+
+        var color = d3.scale.cubehelix();
 
         var links = data.edges;
         var nodes = data.nodes;
@@ -21,13 +23,32 @@ $(document).ready(function() {
 
         force.links(links)
             .nodes(nodes)
+            .linkStrength(5)
             .start();
+
+        // svg.append("defs").selectAll("marker")
+        //     .data(["suit", "licensing", "resolved"])
+        //     .enter().append("marker")
+        //     .attr("id", function(d) {
+        //         return d;
+        //     })
+        //     .attr("viewBox", "0 -5 10 10")
+        //     .attr("refX", 25)
+        //     .attr("refY", 0)
+        //     .attr("markerWidth", 6)
+        //     .attr("markerHeight", 6)
+        //     .attr("orient", "auto")
+        //     .append("path")
+        //     .attr("d", "M0,-5L10,0L0,5 L10,0 L0, -5")
+        //     .style("stroke", "#4679BD")
+        //     .style("opacity", "0.6");
 
         var link = svg.selectAll('.line')
             .data(links)
             .enter()
             .append('line')
             .attr('class', 'link');
+            // .style("marker-end", "url(#suit)");
 
         var node = svg.append('g')
             .attr('class', 'node')
@@ -38,13 +59,26 @@ $(document).ready(function() {
             .attr('r', 5)
             .call(force.drag);
 
+        function compare(a, b) {
+            if (a.source.index < b.source.index) {
+                return -1;
+            }
+            else if (a.source.index > b.source.index) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+
+        var sorted_links = links;
+        sorted_links.sort(compare);
+
+        console.log(sorted_links);
+
         node.on('mouseover', function(d) {
             d3.select(this)
                 .attr('r', 10);
-
-            d.links.forEach(function(l) {
-                l.target.selection.attr('r', 8);
-            })
         })
 
         node.on('mouseout', function(d) {
@@ -60,7 +94,9 @@ $(document).ready(function() {
             .append('text')
             .attr('dx', 30)
             .attr('dy', '.5em')
-            .text(function(d) {return d.name});
+            .text(function(d) {
+                return d.name
+            });
 
         var text_offset = {
             x: 10,
@@ -68,14 +104,30 @@ $(document).ready(function() {
         }
 
         force.on('tick', function() {
-            node.attr('cx', function(d) {return d.x;})
-                .attr('cy', function(d) {return d.y;});
-            link.attr('x1', function(d) {return d.source.x;})
-                .attr('y1', function(d) {return d.source.y;})
-                .attr('x2', function(d) {return d.target.x;})
-                .attr('y2', function(d) {return d.target.y;});
-            text.attr('dx', function(d) {return d.x + text_offset.x;})
-                .attr('dy', function(d) {return d.y + text_offset.y;})
+            node.attr('cx', function(d) {
+                    return d.x;
+                })
+                .attr('cy', function(d) {
+                    return d.y;
+                });
+            link.attr('x1', function(d) {
+                    return d.source.x;
+                })
+                .attr('y1', function(d) {
+                    return d.source.y;
+                })
+                .attr('x2', function(d) {
+                    return d.target.x;
+                })
+                .attr('y2', function(d) {
+                    return d.target.y;
+                });
+            text.attr('dx', function(d) {
+                    return d.x + text_offset.x;
+                })
+                .attr('dy', function(d) {
+                    return d.y + text_offset.y;
+                })
         });
-    }
+    };
 })
