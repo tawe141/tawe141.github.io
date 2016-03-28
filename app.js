ae8c0ae60684a138b1381f02a318bc74738a12f1
Eric <erictaw2018@u.northwestern.edu>
2016-03-27 22:48:33 -0500

    add title and mouseover

--- app.js
+++ app.js
@@ -1,47 +1,81 @@
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'data.json', true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-
-        }
-    }
-    xobj.send(null);
-}
-
-loadJSON(function(response) {
-    var data = JSON.parse(response);
-    var nodes = data.nodes;
-    var links = data.links;
-    var width = 1280;
-    var height = 720;
-    var svg = d3.select('body').append('svg')
-        .attr('width', width)
-        .attr('height', height);
-    var force = d3.layout.force()
-        .size([width, height])
-        .nodes(nodes)
-        .links(links)
-        .linkDistance(50);
-    var link = svg.selectAll('.link')
-        .data(links)
-        .enter().append('line')
-        .attr('class', 'link');
-    var node = svg.selectAll('.node')
-        .data(nodes)
-        .enter().append('circle')
-        .attr('class', 'node');
-    force.on('end', function() {
-        node.attr('r', 10)
-            .attr('cx', function(d) { return d.x; })
-            .attr('cy', function(d) { return d.y; });
-        link.attr('x1', function(d) { return d.source.x; })
-            .attr('y1', function(d) { return d.source.y; })
-            .attr('x2', function(d) { return d.target.x; })
-            .attr('y2', function(d) { return d.target.y; });
-    force.start();
+$(document).ready(function() {
+    $.getJSON('data.json', function(json) {
+        console.log(json);
+        var data = json;
+        visualize(data);
     });
+
+    function visualize(data) {
+        var width = 960;
+        height = 500;
+
+        var links = data.edges;
+        var nodes = data.nodes;
+
+        var force = d3.layout.force()
+            .size([width, height]);
+
+        var svg = d3.select('#app').append('svg')
+            .attr('width', width)
+            .attr('height', height);
+
+        force.links(links)
+            .nodes(nodes)
+            .start();
+
+        var link = svg.selectAll('.line')
+            .data(links)
+            .enter()
+            .append('line')
+            .attr('class', 'link');
+
+        var node = svg.append('g')
+            .attr('class', 'node')
+            .selectAll('circle')
+            .data(nodes)
+            .enter()
+            .append('circle')
+            .attr('r', 5)
+            .call(force.drag);
+
+        node.on('mouseover', function(d) {
+            d3.select(this)
+                .attr('r', 10);
+
+            d.links.forEach(function(l) {
+                l.target.selection.attr('r', 8);
+            })
+        })
+
+        node.on('mouseout', function(d) {
+            d3.select(this)
+                .attr('r', 6)
+        })
+
+        var text = svg.append('g')
+            .attr('class', 'labels')
+            .selectAll('text')
+            .data(force.nodes())
+            .enter()
+            .append('text')
+            .attr('dx', 30)
+            .attr('dy', '.5em')
+            .text(function(d) {return d.name});
+
+        var text_offset = {
+            x: 10,
+            y: 4
+        }
+
+        force.on('tick', function() {
+            node.attr('cx', function(d) {return d.x;})
+                .attr('cy', function(d) {return d.y;});
+            link.attr('x1', function(d) {return d.source.x;})
+                .attr('y1', function(d) {return d.source.y;})
+                .attr('x2', function(d) {return d.target.x;})
+                .attr('y2', function(d) {return d.target.y;});
+            text.attr('dx', function(d) {return d.x + text_offset.x;})
+                .attr('dy', function(d) {return d.y + text_offset.y;})
+        });
+    }
 })
